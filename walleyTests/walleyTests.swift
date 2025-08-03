76f0e5f01ae2a53c03855af464e01009d30f960e
Kinsha Abid <kinsha.abid@gmail.com>
2025-08-03 18:37:30 +0400

    Initial Commit

---
+++ walleyTests/walleyTests.swift
@@ -0,0 +1,16 @@
+//
+//  walleyTests.swift
+//  walleyTests
+//
+//  Created by kinsha on 03/08/25.
+//
+
+import Testing
+
+struct walleyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
